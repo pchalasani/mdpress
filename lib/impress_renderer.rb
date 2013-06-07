a41ae7dc893d22db854ecaf90ac72cf1b7ae9136
Prasad Chalasani <pchalasani@gmail.com>
2013-06-07 19:41:38 -0400

    impress_renderer.rb: mathjax

--- lib/impress_renderer.rb
+++ lib/impress_renderer.rb
@@ -41,9 +41,20 @@ class ImpressRenderer < Redcarpet::Render::HTML
   def mathjax
     if @@opts[:latex]
       %{
-        <script type="text/x-mathjax-config">
-          MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
-        </script>
+<script type="text/x-mathjax-config">
+MathJax.Hub.Config({
+  jax: ["input/TeX", "output/HTML-CSS"],
+  tex2jax: {
+    inlineMath: [ ['$', '$'], ['\\(', '\\)'] ],
+    displayMath: [ ['$$', '$$']],
+    processEscapes: true,
+    skipTags: ['script', 'noscript', 'style', 'textarea', 'pre',
+  'code']
+  },
+  messageStyle: "none",
+  "HTML-CSS": { preferredFont: "TeX", availableFonts: ["STIX","TeX"] }
+});
+</script>
         <script type="text/javascript"
           src="https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
         </script>
